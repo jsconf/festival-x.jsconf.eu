090aa762be2f4f978113d958ecc72f0cd3148e52
Malte Ubl <cramforce@users.noreply.github.com>
2018-10-29 06:55:25 -0700

    Minimum viable website (#7)
    - Last year's homepage template.
    - Created sponsorship inquiry page (copied from 2018)
    - Added links to newsletter registration and 2018 videos to main navigation while we have nothing better.
    - Changed newsletter registration links to https.
    - Added privacy policy link.
    - Updated workbox version and added support for navigation preload.

--- contents/js/main.js
+++ contents/js/main.js
@@ -24,3 +24,18 @@ function pingTypekit() {
     new Image().src = url
   }
 }
+
+function prefetch() {
+  addLink('prefetch', 'https://ti.to/jsconfeu/jsconf-eu-x-2019');
+  addLink('prefetch', 'https://use.typekit.com/zjo8qhc.js');
+  addLink('prefetch', 'https://assets.beta.tito.io/assets/event/mcdonagh-f6f1b3c7a80e57c984a0f9d9c4b8b85028edc3ea3fc9a6526da63316cb314e3c.css');
+  addLink('prefetch', 'https://js.stripe.com/v2/');
+  addLink('prefetch', 'https://eepurl.com/dvQxb5');
+}
+
+function addLink(rel, href) {
+  const link = document.createElement('link');
+  link.rel = rel;
+  link.href = href;
+  document.head.appendChild(link);
+}
--- contents/sw.js
+++ contents/sw.js
@@ -1,4 +1,6 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+workbox.navigationPreload.enable();
 
 // Uncomment for debugging
 // workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
@@ -27,26 +29,28 @@ workbox.routing.registerRoute(
   })
 );
 
-// Avoid the doc cache during local dev, because the file
-// generation never wins against the 1 second deadline.
-if (!(/localhost/.test(origin))) {
-  workbox.routing.registerRoute(
-    // Prefer the network but if it doesn't respond within 1 seconds,
-    // fallback to a doc if we have a cached version that is max
-    // 3 days old.
-    // Basically that means that the schedule should load offline for the
-    // duration of the conference.
-    /(\/|\.html)$/,
-    // Use the network unless things are slow
-    workbox.strategies.networkFirst({
-      cacheName: 'doc-cache',
-      networkTimeoutSeconds: 1,
-      plugins: [
-        new workbox.expiration.Plugin({
-          // Cache for a maximum of three days
-          maxAgeSeconds: 3 * 24 * 60 * 60,
-        })
-      ],
-    })
-  );
+let networkTimeoutForNavigationsSeconds = 2;
+// Allow the server plenty of time to generate responses in local dev.
+if ((/localhost/.test(origin))) {
+  networkTimeoutForNavigationsSeconds = 20;
 }
+
+// Prefer the network but if it doesn't respond within 1 seconds,
+// fallback to a doc if we have a cached version that is max
+// 3 days old.
+// Basically that means that the schedule should load offline for the
+// duration of the conference.
+const navigationStrategy = workbox.strategies.networkFirst({
+  cacheName: 'nav-cache',
+  // Use the network unless things are slow
+  networkTimeoutSeconds: networkTimeoutForNavigationsSeconds,
+  plugins: [
+    new workbox.expiration.Plugin({
+      // Cache for a maximum of three days
+      maxAgeSeconds: 3 * 24 * 60 * 60,
+    })
+  ],
+});
+
+const navigationRoute = new workbox.routing.NavigationRoute(navigationStrategy, {});
+workbox.routing.registerRoute(navigationRoute);
