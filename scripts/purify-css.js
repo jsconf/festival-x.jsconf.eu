d3d04917eeb73dc8e198f11495be63aac5faab52
Malte Ubl <malte.ubl@gmail.com>
2018-11-20 19:18:13 -0800

    Inline CSS into each page and minify it specifically for that page.

--- scripts/purify-css.js
+++ scripts/purify-css.js
@@ -3,45 +3,58 @@ const find = require('find');
 const path = require('path');
 const fs = require('fs');
 const replace = require('replace-in-file');
+const {promisify} = require('util');
 
-const content = ['build/**/*.js', 'build/**/*.html'];
+preMinifyCss()
 
+function preMinifyCss() {
+  find.file(/main\.css$/, 'build/', cssFiles => {
+    if (cssFiles.length != 1) {
+      throw new Error('Expected exactly 1 css file: ' + cssFiles.join(', '));
+    }
+    console.log('Input CSS', cssFiles[0]);
 
-find.file(/main\.css$/, 'build/', files => {
-  if (files.length != 1) {
-    throw new Error('Expected exactly 1 css file: ' + files.join(', '));
-  }
-  console.log('Purifying ' + files);
-  purifycss(content, files, {
-    // Comment in for debugging.
-    // rejected: true,
-    minify: true,
-    info: true,
-    whitelist: ['*md-content*'],
-  }, css => {
-    // Change in-place
-    console.log('Done purifying');
-    rehash(files[0], css);
+    // First minify everything together. This makes the per file minify
+    // drastically faster, because we throw away 90% of the CSS here.
+    purifycss(['build/js/main.js', 'build/**/*.html'], cssFiles, {
+      whitelist: ['*md-content*'],
+      // info: true,
+    }, async function(css) {
+      console.info('Preprocessed CSS');
+      processHtml(css);
+    });
   });
-});
+}
 
-// Rehash because now new HTML files can change what CSS we need to prune
-function rehash(filename, css) {
-  const hash = require('crypto').createHash('sha1');
-  hash.update(css, 'utf8');
-  const digest = hash.digest('hex');
-  fs.writeFileSync(filename, css);
-  const parts = filename.split(path.sep);
-  const oldDigest = parts[2];
-  const prefix = parts[0] + path.sep + parts[1];
-  const oldDir = prefix + path.sep + oldDigest;
-  const newDir = prefix + path.sep + digest;
-  console.log('Renaming', oldDir, 'to', newDir);
-  fs.renameSync(oldDir, newDir);
-  // Replace the references to the digest in the HTML files.
-  console.log('Replacing digest', replace.sync({
-    files: 'build/**/*.html',
-    from: oldDigest,
-    to: digest,
-  }));
+function processHtml(baseCss) {
+  find.file(/\.html$/, 'build/', htmls => {
+    htmls.forEach(async function(htmlFilename) {
+      console.log('Processing', htmlFilename);
+      let html = await promisify(fs.readFile)(htmlFilename, 'utf8');
+      if (/noindex/.test(html)) {
+        console.info('Skipping placeholder', htmlFilename);
+        return;
+      }
+      purifycss(['build/js/main.js', htmlFilename], baseCss, {
+        // Comment in for debugging.
+        // rejected: true,
+        minify: true,
+        // info: true,
+        whitelist: ['*md-content*'],
+      }, async function(css) {
+        console.log('Done purifying', htmlFilename);
+        let html = await promisify(fs.readFile)(htmlFilename, 'utf8');
+        let found = false;
+        html = html.replace(/<link href=\/immutable\/\w+\/css\/main.css rel=stylesheet>/, match => {
+          found = true;
+          return '<style>' + css + '</style>';
+        });
+        if (!found) {
+          console.error('Cant find link element in ', html);
+          throw new Error('Fail');
+        }
+        await promisify(fs.writeFile)(htmlFilename, html);
+      });
+    });
+  });
 }
