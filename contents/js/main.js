56bf6311bbbb3c968cc39d50022bde7b1bcfa1c8
Lukasz Klis <lukasz@klis.pl>
2018-11-30 22:11:54 +0000

    Add basic styling for the landing page (#8)

--- contents/js/main.js
+++ contents/js/main.js
@@ -40,3 +40,45 @@ function addLink(rel, href) {
   link.href = href;
   document.head.appendChild(link);
 }
+
+Array.from(document.querySelectorAll('.inline-video-link')).forEach(function(link) {
+  link.onclick = function(e) {
+    e.preventDefault();
+    var iframe = document.createElement('iframe');
+    iframe.src = 'https://www.youtube-nocookie.com/embed/zNk28iHdoFk?autoplay=1'
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allowfullscreen', 'true');
+    iframe.setAttribute('allow', 'autoplay https://www.youtube-nocookie.com/');
+    iframe.className = 'section-video';
+    iframe.style.padding = 0;
+    link.parentElement.replaceChild(iframe, link);
+  };
+});
+
+function introAnimation(intro) {
+  var anim1 = bodymovin.loadAnimation({
+    container: intro,
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    animationData: window.festivalXIntroAnimationJson
+  });
+
+  anim1.addEventListener("DOMLoaded", el => {
+    anim1.addEventListener("complete", el => {
+      anim1.playSegments([259, 396], true);
+    });
+  });
+}
+var intro = document.getElementById("intro");
+if (intro) {
+  // The script contains both the lottie framework and the
+  // window.festivalXIntroAnimationJson variable that contains
+  // the actual animation.
+  var script = document.createElement('script');
+  script.src = intro.getAttribute('data-src');
+  script.onload = function() {
+    introAnimation(intro);
+  };
+  document.head.appendChild(script);
+}
