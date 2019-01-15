7821aacb74a48844b45036b3dc9b487439a2bf19
Malte Ubl <cramforce@users.noreply.github.com>
2019-01-14 21:28:01 -0800

    When possible make images responsive without reflow. (#41)

--- templates/filters/contentUrls.js
+++ templates/filters/contentUrls.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 
 // Replace URLs of the form
 // contents:images/foo.jpg
@@ -8,8 +9,25 @@ module.exports = function(html, contents) {
     const parts = path.split('/');
     let image = contents;
     parts.forEach(part => {
+      if (!image) {
+        return;
+      }
       image = image[part];
     });
-    return image.url;
-  });
+    if (!image) {
+      const message = 'Can\'t find image: ' + url;
+      console.error(chalk.red.bold(message));
+      throw new Error(message);
+    }
+    let transformed = image.url;
+    if (image.getImageInfo) {
+      transformed += '#ar=' + image.getImageInfo().aspectPercentage
+    }
+    return transformed;
+  })
+  // Wrap images that are their own paragraph with a responsive wrapper
+  // to avoid reflow on load.
+  .replace(/<p>(<img[^>]+#ar=(\d+)[^>]*>)<\/p>/g, function(all, img, aspectPercentage) {
+    return `<p><span class="responsive-image-wrapper" style="padding-top:${aspectPercentage}%">${img}</span></p>`
+  })
 }
