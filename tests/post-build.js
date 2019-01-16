1f6cfe726cba7943d590d219bcc292f183931424
Malte Ubl <malte.ubl@gmail.com>
2019-01-16 06:52:36 -0800

    Add additional tests to ensure successful travis builds.

--- tests/post-build.js
+++ tests/post-build.js
@@ -11,15 +11,22 @@ function checkDir(name, minCount) {
   var count = fs.readdirSync(name).length;
   if (count < minCount) {
     fail(`Unexpected low count of files in ${name}: ${count} < ${minCount}. Was there a problem in the spreadsheet import?`);
+  } else {
+    console.info(`PASS: ${name}: ${count} >= ${minCount}`);
   }
 }
 
 // checkDir('contents/speakers/', 0);
 // checkDir('build/speakers/', 0);
-// checkDir('contents/sponsors/', 0);
 // checkDir('contents/mcs/', 0);
 checkDir('contents/team/', 3);
 checkDir('build/team/', 3);
 checkDir('contents/news/', 4);
 checkDir('build/news/', 4);
 checkDir('contents/images/cms/', 3);
+checkDir('contents/sponsors/', 3);
+checkDir('build/sponsors/', 3);
+// Standlone content.
+checkDir('build/about/', 1);
+checkDir('build/why/', 1);
+checkDir('build/x/', 1);
