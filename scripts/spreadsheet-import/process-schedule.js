60e5933691f4564ec080fcd275b4ca1878444883
Malte Ubl <malte.ubl@gmail.com>
2019-05-25 03:36:18 -0700

    Switch in-venue schedule to use checksum to check for changes and generate checksum for json.
    Escuse the diff. Prettier's fault.

--- scripts/spreadsheet-import/process-schedule.js
+++ scripts/spreadsheet-import/process-schedule.js
@@ -1,50 +1,76 @@
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports.processSchedule = function(sheet) {
-  const {data, speakers} = structureData(sheet);
+  const { data, speakers } = structureData(sheet);
+  const hash = require("crypto").createHash("sha1");
+  hash.update(JSON.stringify({ schedule: data, speakers }));
   const schedule = {
-    info: info(),
+    info: info(hash.digest("hex")),
     schedule: data,
-    speakers,
-  }
+    speakers
+  };
   // Write with .txt filename, because wintersmith doesn't support serving
   // files with the "magic" .json extension.
-  fs.writeFileSync('./contents/schedule.json', JSON.stringify({
-    "pageId": "scheduleJson",
-    "template": "pages/json.html.njk",
-    "filename": ":file.json",
-    "actualJson": schedule,
-  }, null, '  '));
-}
+  fs.writeFileSync(
+    "./contents/schedule.json",
+    JSON.stringify(
+      {
+        pageId: "scheduleJson",
+        template: "pages/json.html.njk",
+        filename: ":file.json",
+        actualJson: schedule
+      },
+      null,
+      "  "
+    )
+  );
+};
 
 const columns = [
-  'backtrack:startTime', 'backtrack:duration', 'backtrack:number',
-  '-', 'backtrack:who',  'backtrack:what', '-',
-  'sidetrack:startTime', 'sidetrack:duration', 'sidetrack:number',
-  '-', 'sidetrack:who',  'sidetrack:what', 
-  'community:startTime', 'community:what', 'community:detail', 
-  '-',   'bipocit:who',   'bipocit:what',
-  '-',    'livejs:what',    'livejs:detail',
-  '-',   'sponsor:what',   'sponsor:detail'
+  "backtrack:startTime",
+  "backtrack:duration",
+  "backtrack:number",
+  "-",
+  "backtrack:who",
+  "backtrack:what",
+  "-",
+  "sidetrack:startTime",
+  "sidetrack:duration",
+  "sidetrack:number",
+  "-",
+  "sidetrack:who",
+  "sidetrack:what",
+  "community:startTime",
+  "community:what",
+  "community:detail",
+  "-",
+  "bipocit:who",
+  "bipocit:what",
+  "-",
+  "livejs:what",
+  "livejs:detail",
+  "-",
+  "sponsor:what",
+  "sponsor:detail"
 ];
 
 const tracksMap = {
-  backtrack: 'Back Track',
-  sidetrack: 'Side Track',
-  community: 'Community Lounge',
-  bipocit: 'BIPoCiT Space',
-  livejs: 'live:js Stage',
-  sponsor: 'Sponsor Booth',
-}
+  backtrack: "Back Track",
+  sidetrack: "Side Track",
+  community: "Community Lounge",
+  bipocit: "BIPoCiT Space",
+  livejs: "live:js Stage",
+  sponsor: "Sponsor Booth"
+};
 
 function structureData(lessCrappyData) {
   let day = 1;
   const mergedRecords = {};
   const speakers = {};
   for (let row = 2, nRows = lessCrappyData.length; row < nRows; row++) {
-
-
-    if (!lessCrappyData[row]) { continue; }
+    if (!lessCrappyData[row]) {
+      continue;
+    }
 
     if (/Day 2:/.test(lessCrappyData[row][0])) {
       day = 2;
@@ -53,21 +79,22 @@ function structureData(lessCrappyData) {
     let rowStartTime = null;
     const tracks = {};
     for (let col = 0, nCols = lessCrappyData[row].length; col < nCols; col++) {
-      if (!columns[col] || columns[col] === '-') { continue; }
-      const [track, field] = columns[col].split(':');
-
+      if (!columns[col] || columns[col] === "-") {
+        continue;
+      }
+      const [track, field] = columns[col].split(":");
 
       if (!tracks[track]) {
         tracks[track] = {
           day: day,
-          date: day == 1 ? '2019-06-01' : '2019-06-02',
+          date: day == 1 ? "2019-06-01" : "2019-06-02",
           track: tracksMap[track],
           trackId: track,
-          startTime: rowStartTime,
+          startTime: rowStartTime
         };
       }
       var val = lessCrappyData[row][col];
-      if (typeof val == 'string') {
+      if (typeof val == "string") {
         val = val.trim();
       }
       tracks[track][field] = val;
@@ -80,12 +107,19 @@ function structureData(lessCrappyData) {
       if (!tracks[track].startTime || !tracks[track].what) {
         return;
       }
-      tracks[track].startTime = String(tracks[track].startTime).replace(':', '.');
-      tracks[track].dateTime = tracks[track].date + ' ' +
-          tracks[track].startTime.replace('.', ':') +
-          ' GMT+0200';
-      tracks[track].id = (tracks[track].who + ' ' + tracks[track].what)
-          .toLowerCase().trim().replace(/[^a-z0-9]/g, '-');
+      tracks[track].startTime = String(tracks[track].startTime).replace(
+        ":",
+        "."
+      );
+      tracks[track].dateTime =
+        tracks[track].date +
+        " " +
+        tracks[track].startTime.replace(".", ":") +
+        " GMT+0200";
+      tracks[track].id = (tracks[track].who + " " + tracks[track].what)
+        .toLowerCase()
+        .trim()
+        .replace(/[^a-z0-9]/g, "-");
       if (tracks[track].who) {
         speakers[tracks[track].who.toLowerCase()] = tracks[track];
       }
@@ -105,11 +139,13 @@ function structureData(lessCrappyData) {
   };
 }
 
-function info() {
+function info(checksum) {
   const now = new Date();
-  const conferenceDay = now < Date.parse('Sun Jun 01 2019 00:00:00 GMT+0200 (CEST)') ? 1 : 2;
+  const conferenceDay =
+    now < Date.parse("Sun Jun 01 2019 00:00:00 GMT+0200 (CEST)") ? 1 : 2;
   return {
     currentDay: conferenceDay,
-    generationTime: now.toString(),
+    checksum,
+    generationTime: now.toString()
   };
 }
