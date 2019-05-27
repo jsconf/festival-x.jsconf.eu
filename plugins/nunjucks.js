ec176af06fad128bd022767729b15d682c109d42
Malte Ubl <malte.ubl@gmail.com>
2019-05-27 08:35:54 +0200

    Create versions of schedule that display only talks from community lounge or bipocit space.

--- plugins/nunjucks.js
+++ plugins/nunjucks.js
@@ -1,8 +1,8 @@
-const nunjucks = require('nunjucks');
-const path = require('path');
-const minify = require('html-minifier').minify;
-const fs = require('fs');
-const imageSize = require('image-size');
+const nunjucks = require("nunjucks");
+const path = require("path");
+const minify = require("html-minifier").minify;
+const fs = require("fs");
+const imageSize = require("image-size");
 
 module.exports = function(env, callback) {
   const optionDefaults = {
@@ -12,22 +12,31 @@ module.exports = function(env, callback) {
     filtermodules: [],
     extensions: {}
   };
-  Object.assign(env.config.locals, require('../locals-generated.json'));
+  Object.assign(env.config.locals, require("../locals-generated.json"));
   env.config.locals.loadSchedule = function(contents) {
-    if (!fs.existsSync('./contents/schedule.json')) {
-      console.error('Error: Schedule file was not generated.')
+    if (!fs.existsSync("./contents/schedule.json")) {
+      console.error("Error: Schedule file was not generated.");
       return null;
     }
-    return JSON.parse(fs.readFileSync('./contents/schedule.json')).actualJson;
+    return JSON.parse(fs.readFileSync("./contents/schedule.json")).actualJson;
+  };
+  env.config.locals.filterSessionsByTrackId = function(sessions, trackId) {
+    const filtered = {};
+    for (let time in sessions) {
+      const talks = sessions[time].filter(t => t.trackId == trackId);
+      if (talks.length > 0) {
+        filtered[time] = talks;
+      }
+    }
+    return filtered;
   };
   env.config.locals.Date = Date;
-  env.config.locals.effectiveUrl = env.mode === 'preview'
-      ? 'http://localhost:8080'
-      : 'https://2019.jsconf.eu';
+  env.config.locals.effectiveUrl =
+    env.mode === "preview" ? "http://localhost:8080" : "https://2019.jsconf.eu";
 
   // Load the new nunjucks environment.
   const loaderOpts = {
-    watch: env.mode === 'preview'
+    watch: env.mode === "preview"
   };
   const loader = new nunjucks.FileSystemLoader(env.templatesPath, loaderOpts);
   const nenv = new nunjucks.Environment(loader);
@@ -36,7 +45,7 @@ module.exports = function(env, callback) {
   const options = Object.assign({}, optionDefaults, env.config.nunjucks);
 
   options.filters.map(function(name) {
-    const file = path.join(options.filterdir, name + '.js');
+    const file = path.join(options.filterdir, name + ".js");
     const filter = env.loadModule(env.resolvePath(file), true);
     nenv.addFilter(name, filter);
   });
@@ -70,7 +79,7 @@ module.exports = function(env, callback) {
         }
         try {
           if (!html) {
-            callback(new Error('Template render failed: ' + this.filename));
+            callback(new Error("Template render failed: " + this.filename));
             return;
           }
           html = minify(html, {
@@ -78,36 +87,41 @@ module.exports = function(env, callback) {
             collapseWhitespace: true,
             removeComments: true,
             sortClassName: true,
-            sortAttributes: true,
+            sortAttributes: true
           });
-          html = html.replace(/___CHECKSUM___/, () =>{
-            const hash = require('crypto').createHash('sha1');
+          html = html.replace(/___CHECKSUM___/, () => {
+            const hash = require("crypto").createHash("sha1");
             hash.update(html);
-            return hash.digest('hex')
+            return hash.digest("hex");
           });
           callback(null, new Buffer(html));
         } catch (error) {
           callback(error);
         }
       });
-
     }
 
     static fromFile(filepath, callback) {
-      callback(null, new NunjucksTemplate(nenv.getTemplate(filepath.relative), filepath.relative));
+      callback(
+        null,
+        new NunjucksTemplate(
+          nenv.getTemplate(filepath.relative),
+          filepath.relative
+        )
+      );
     }
   }
 
-  nenv.addFilter('notDraft', function(array) {
+  nenv.addFilter("notDraft", function(array) {
     if (!array) {
       return [];
     }
     return array.filter(function(item) {
       return !item.metadata.draft;
-    })
+    });
   });
 
-  nenv.addFilter('archive', function(array, filterBool) {
+  nenv.addFilter("archive", function(array, filterBool) {
     if (!array) {
       return [];
     }
@@ -116,37 +130,37 @@ module.exports = function(env, callback) {
     });
   });
 
-  nenv.addFilter('notRedirect', function(array) {
+  nenv.addFilter("notRedirect", function(array) {
     if (!array) {
       return [];
     }
     return array.filter(function(item) {
       return !item.metadata.redirectUrl;
-    })
+    });
   });
 
-  nenv.addFilter('json', function(object) {
+  nenv.addFilter("json", function(object) {
     return JSON.stringify(object);
   });
 
-  nenv.addFilter('seededStableRandom', function(array, seed) {
-    const rand = require('seed-random')(seed);
+  nenv.addFilter("seededStableRandom", function(array, seed) {
+    const rand = require("seed-random")(seed);
     const index = Math.floor(rand() * array.length);
     return array[index];
   });
 
-  env.registerTemplatePlugin('**/*.*(html|nunjucks|njk)', NunjucksTemplate);
+  env.registerTemplatePlugin("**/*.*(html|nunjucks|njk)", NunjucksTemplate);
   // Transform static URLs into the form:
   // /immutable/$fileHash/filename
-  env.plugins.StaticFile.prototype.getFilename = function getFilename () {
+  env.plugins.StaticFile.prototype.getFilename = function getFilename() {
     // Top level files such as CNAME and manifest.json should not be renamed.
-    if (!(/\//.test(this.filepath.relative))) {
+    if (!/\//.test(this.filepath.relative)) {
       return this.filepath.relative;
     }
-    const hash = require('crypto').createHash('sha1');
-    hash.update(require('fs').readFileSync(this.filepath.full), 'utf8');
-    return 'immutable/' + hash.digest('hex') + '/' + this.filepath.relative;
-  }
+    const hash = require("crypto").createHash("sha1");
+    hash.update(require("fs").readFileSync(this.filepath.full), "utf8");
+    return "immutable/" + hash.digest("hex") + "/" + this.filepath.relative;
+  };
   env.plugins.StaticFile.prototype.getImageInfo = function() {
     if (this.imageInfo_) {
       return this.imageInfo_;
@@ -156,19 +170,24 @@ module.exports = function(env, callback) {
     const size = imageSize(buffer);
     this.imageInfo_.width = size.width;
     this.imageInfo_.height = size.height;
-    this.imageInfo_.aspectPercentage = Math.round((size.height / size.width) * 100);
+    this.imageInfo_.aspectPercentage = Math.round(
+      size.height / size.width * 100
+    );
     return this.imageInfo_;
-  }
+  };
   // OMG, super hacky way to make local server emit a long max-age. This dramatically
   // speeds up the dev server responses since all resources are cached unless they
   // actually changed (in that case the URL changes and hence there is no cache hit).
-  const writeHead = require('http').ServerResponse.prototype.writeHead;
-  require('http').ServerResponse.prototype.writeHead = function(status, headers) {
-    let maxAge = '8640000';
-    if (headers['Content-Type'].startsWith('text/html')) {
-      maxAge = '0'
+  const writeHead = require("http").ServerResponse.prototype.writeHead;
+  require("http").ServerResponse.prototype.writeHead = function(
+    status,
+    headers
+  ) {
+    let maxAge = "8640000";
+    if (headers["Content-Type"].startsWith("text/html")) {
+      maxAge = "0";
     }
-    headers['cache-control'] = 'public, max-age=' + maxAge;
+    headers["cache-control"] = "public, max-age=" + maxAge;
     writeHead.call(this, status, headers);
   };
   callback();
