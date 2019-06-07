e6e079463a1842d510fd80f8510e74f3cb550b2f
Malte Ubl <malte.ubl@gmail.com>
2019-06-07 14:35:17 -0700

    Switch to using YT API instead of RSS.
    And use YT poster for video pages

--- scripts/spreadsheet-import/video-feed.js
+++ scripts/spreadsheet-import/video-feed.js
@@ -1,30 +1,50 @@
-let Parser = require("rss-parser");
-let parser = new Parser({
-  customFields: {
-    item: [["media:group", "media"]]
-  }
-});
+const fetch = require("node-fetch");
+
+// New API URL
+//
 
 // Downloads the YT playlist and automatically maps talks to their
 // respective video through the talk URL pasted into the YT description.
 exports.getVideoMapping = async function() {
-  let feed = await parser.parseURL(
-    "https://www.youtube.com/feeds/videos.xml?playlist_id=PL37ZVnwpeshHwJPVBqEnZild7QHWhdufu"
-  );
-
   const videos = {};
-  feed.items.forEach(item => {
+  return get(videos);
+};
+
+async function get(videos, token) {
+  let url =
+    "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PL37ZVnwpeshHwJPVBqEnZild7QHWhdufu&key=" +
+    process.env.youtube_api_key;
+  if (token) {
+    url += "&pageToken=" + token;
+  }
+  const response = await fetch(url);
+  const json = await response.json();
+
+  processYt(videos, json);
+  if (json.nextPageToken) {
+    await get(videos, json.nextPageToken);
+  }
+  return videos;
+}
+
+function processYt(videos, json) {
+  json.items.forEach(item => {
+    const snippet = item.snippet;
+    const id = snippet.resourceId.videoId;
+    if (!id) {
+      throw new Error(`Can't find video id in ${JSON.stringify(item)}`);
+    }
     const yt = {
-      id: item.id.split(":")[2],
-      url: item.link,
-      title: item.title
+      id: id,
+      url: "https://youtube.com/watch/" + encodeURIComponent(id),
+      title: snippet.title,
+      poster: snippet.thumbnails.maxres.url
     };
-    const desc = item.media["media:description"][0];
+    const desc = snippet.description;
     const url = desc.match(/https\:\/\/2019\.jsconf\.eu(\/\S+\.html)/m);
     if (url) {
       yt.websiteUrl = url[1];
       videos[url[1]] = yt;
     }
   });
-  return videos;
-};
+}
