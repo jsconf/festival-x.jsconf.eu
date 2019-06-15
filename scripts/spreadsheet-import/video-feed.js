cce774c1cd5f3c93dff7f23a5d60e8ac865b50f0
Malte Ubl <malte.ubl@gmail.com>
2019-06-15 12:18:27 -0700

    Sort YT videos based on the playlist. Indicate videos with blurry placeholder.

--- scripts/spreadsheet-import/video-feed.js
+++ scripts/spreadsheet-import/video-feed.js
@@ -6,11 +6,10 @@ const fetch = require("node-fetch");
 // Downloads the YT playlist and automatically maps talks to their
 // respective video through the talk URL pasted into the YT description.
 exports.getVideoMapping = async function() {
-  const videos = {};
-  return get(videos);
+  return get({}, 0, null);
 };
 
-async function get(videos, token) {
+async function get(videos, index, token) {
   let url =
     "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PL37ZVnwpeshHwJPVBqEnZild7QHWhdufu&key=" +
     process.env.youtube_api_key;
@@ -20,14 +19,14 @@ async function get(videos, token) {
   const response = await fetch(url);
   const json = await response.json();
 
-  processYt(videos, json);
+  index = processYt(videos, json, index);
   if (json.nextPageToken) {
-    await get(videos, json.nextPageToken);
+    await get(videos, index, json.nextPageToken);
   }
   return videos;
 }
 
-function processYt(videos, json) {
+function processYt(videos, json, index) {
   json.items.forEach(item => {
     const snippet = item.snippet;
     const id = snippet.resourceId.videoId;
@@ -38,7 +37,8 @@ function processYt(videos, json) {
       id: id,
       url: "https://youtube.com/watch/" + encodeURIComponent(id),
       title: snippet.title,
-      poster: snippet.thumbnails.maxres.url
+      poster: snippet.thumbnails.maxres.url,
+      index: index++
     };
     const desc = snippet.description;
     const url = desc.match(/https\:\/\/2019\.jsconf\.eu(\/\S+\.html)/m);
@@ -47,4 +47,5 @@ function processYt(videos, json) {
       videos[url[1]] = yt;
     }
   });
+  return index;
 }
